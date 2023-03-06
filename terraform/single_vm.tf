d9886004b6073ce0c7023bc8d93a3daee3249512
joselu55555 <41442595+joselu55555@users.noreply.github.com>
2023-03-06 11:01:13 +0100

    Update single_vm.tf

--- terraform/single_vm.tf
+++ terraform/single_vm.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "joselu" {
   name     = var.resource_group_name
   location = var.location_name
 }
@@ -6,21 +6,21 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = var.network_name
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.joselu.location
+  resource_group_name = azurerm_resource_group.joselu.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = azurerm_resource_group.joselu.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "vnic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.joselu.location
+  resource_group_name = azurerm_resource_group.joselu.name
 
   ip_configuration {
     name                          = "internal"
@@ -29,10 +29,19 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
+
+resource "azurerm_public_ip" "pip" {
+  name                = "VIP"
+  location            = azurerm_resource_group.joselu.location
+  resource_group_name = azurerm_resource_group.joselu.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm1"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.joselu.name
+  location            = azurerm_resource_group.joselu.location
   size                = "Standard_F2"
   admin_username      = "azureuser"
   network_interface_ids = [
@@ -41,10 +50,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/root/keys_SSH/azure.pub")
   }
-
-  os_disk {
+  
+	  os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
@@ -55,13 +64,47 @@ resource "azurerm_linux_virtual_machine" "vm" {
     publisher = "cognosys"
   }
 
-
   source_image_reference {
     publisher = "cognosys"
     offer     = "centos-8-stream-free"
     sku       = "centos-8-stream-free"
     version   = "22.03.28"
   }
+}
+  
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "joselu_ssh" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.joselu.location
+  resource_group_name = azurerm_resource_group.joselu.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"	
+	}
+	security_rule {
+    name                       = "http"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"	  
+    }
+}	
+	
+resource "azurerm_subnet_network_security_group_association" "joselu_ssh" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.joselu_ssh.id
 }
 
 
