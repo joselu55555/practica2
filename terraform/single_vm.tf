07c476a002eb69192fdb391d900f72c60b339fe5
joselu55555 <41442595+joselu55555@users.noreply.github.com>
2023-03-07 17:14:01 +0100

    Update single_vm.tf

--- terraform/single_vm.tf
+++ terraform/single_vm.tf
@@ -115,3 +115,24 @@ resource "azurerm_container_registry" "containerRegistryjoselu" {
   sku                 = "Premium"
   admin_enabled       = false
    }
+
+resource "azurerm_kubernetes_cluster" "joselu_cluster" {
+  name                = "joselu-aks1"
+  location            = azurerm_resource_group.joselu.location
+  resource_group_name = azurerm_resource_group.joselu.name
+  dns_prefix          = "practica2aks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production_devops"
+  }
+}
