36de868989a503c58a30118cdd57a0efaea032f9
joselu55555 <41442595+joselu55555@users.noreply.github.com>
2023-03-08 08:41:10 +0100

    Update single_vm.tf

--- terraform/single_vm.tf
+++ terraform/single_vm.tf
@@ -26,6 +26,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
+	public_ip_address_id = azurerm_public_ip.pip.id
   }
 }
 
